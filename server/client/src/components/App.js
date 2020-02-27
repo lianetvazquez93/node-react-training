5c366006ce7af33be6dd2cbcb7e1432b73249e1b
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-02-27 12:17:46 +0100

    * Redux Thunk  * Refactoring the App  * Testing FetchUser

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,23 +1,31 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
+import * as actions from "../actions";
 import Header from "./Header";
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/surveys" component={Dashboard} />
-        <Route path="/surveys/new" component={SurveyNew} />
-      </div>
-    </BrowserRouter>
-  );
-};
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
