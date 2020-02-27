1f36c2b32e1efb98f228e9df9a254d3cd415d295
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-02-27 17:07:14 +0100

    Header content

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -5,11 +5,19 @@ class Header extends React.Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
-        return "Still deciding";
+        return;
       case false:
-        return "I am logged out";
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
       default:
-        return "I am logged in";
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
     }
   }
 
