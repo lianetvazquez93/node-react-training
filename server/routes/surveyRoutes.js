1f0eeac14b7c5df3dead227fe6b114681dc445c5
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-03-10 11:20:43 +0100

    Polish in the route handler

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -22,6 +22,16 @@ module.exports = app => {
 
     //Great place to send an email
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
