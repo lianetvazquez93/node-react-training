99c678552c435ba03b4ecec5e59c3fce474cdad3
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-03-11 18:09:19 +0100

    Styling the form

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import React from "react";
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
 
 const FIELDS = [
@@ -22,7 +23,13 @@ class SurveyForm extends React.Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat left white-text">
+            Cancel
+          </Link>
+          <button className="teal btn-flat right white-text" type="submit">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
