4c7f71f24707f83996069c74b16043064bd071fe
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-03-13 17:41:34 +0100

    * Posting to surveys  * Redirect on submit

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,11 @@
 import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 import formFields from "./formFields";
 import * as actions from "../../actions";
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={name}>
@@ -21,7 +22,10 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
       <button className="yellow white-text darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
-      <button className="green white-text btn-flat right" onClick={() => submitSurvey(formValues)}>
+      <button
+        className="green white-text btn-flat right"
+        onClick={() => submitSurvey(formValues, history)}
+      >
         Send Survey
         <i className="material-icons right">email</i>
       </button>
@@ -33,4 +37,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
